959ebac79ff77cd0aa9917be9f6510aa90dd636e
cen5bin <cen5bin@163.com>
2015-05-07 09:40:46 +0800

    登录界面完善

--- Chrome-Extension/popup.js
+++ Chrome-Extension/popup.js
@@ -20,18 +20,57 @@ function get_user_data() {
     return ret;
 }
 
+//验证邮箱格式
+function is_email(str) {
+    if (str.charAt(0) == "." || str.charAt(0) == "@" || str.indexOf('@', 0) == -1 ||
+        str.indexOf('.', 0) == -1 || str.lastIndexOf("@") == str.length - 1 ||
+        str.lastIndexOf(".") == str.length - 1 ||
+        str.indexOf('@.') > -1)
+        return false;
+    else
+        return true;
+}
+
+function clear_error_tip() {
+    $('.input-container p').remove();
+}
+
+//检查用户输入的数据是否合法
+function judge_user_data_valid() {
+    clear_error_tip();
+    var data = get_user_data();
+    if (data['email'].length == 0) {
+        $($('.input-container')[0]).append('<p>邮箱不能为空</p>');
+        return false;
+    }
+    if (data['password'].length < 6) {
+        $($('.input-container')[1]).append('<p>密码不得少于6位</p>')
+        return false;
+    }
+    if (!is_email(data['email'])) {
+        $($('.input-container')[0]).append('<p>非法邮箱</p>');
+        return false;
+    }
+    return true;
+}
 
 //登录功能
 function sign_in() {
-    chrome.extension.getBackgroundPage().send_request_post('SignIn', get_user_data(), function(data){
-        console.log(data.zz);
+    if (!judge_user_data_valid()) return;
+    chrome.runtime.getBackgroundPage(function(background){
+         background.send_request_post('SignIn', get_user_data(), function(data){
+             console.log(data.zz);
+         });
     });
 }
 
 //注册功能
 function sign_up() {
-    chrome.extension.getBackgroundPage().send_request_post('SignUp', get_user_data(), function(data){
-        console.log(data);
+    if (!judge_user_data_valid()) return;
+    chrome.runtime.getBackgroundPage(function(background){
+        background.send_request_post('SignUp', get_user_data(), function(data){
+            console.log(data);
+        });
     });
 }
 
